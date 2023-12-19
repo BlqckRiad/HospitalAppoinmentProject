a11a13359dd7cad498725c4c9490ea2baefb2719
BlqckRiad <enesozbuganli36@gmail.com>
2023-12-19 15:01:05 +0300

    Program.cs

---
+++ Program/Program.cs
@@ -0,0 +1,109 @@
+﻿using HospitalApp;
+using HospitalApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<HospitalContext>(options => options.UseSqlite(builder.Configuration["ConnectionStrings:Hospital_Connection"]));
+
+
+builder.Services.AddControllersWithViews().AddViewLocalization(
+        Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix
+    );
+builder.Services.AddLocalization(opt =>
+{
+    opt.ResourcesPath = "Resources";
+});
+builder.Services.Configure<RequestLocalizationOptions>(opt =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo ("en-US"),
+        new CultureInfo ("tr-TR"),
+    };
+
+    opt.DefaultRequestCulture = new RequestCulture ("tr-TR");
+    opt.SupportedUICultures = supportedCultures;
+    opt.SupportedCultures = supportedCultures;
+    
+});
+
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlite(builder.Configuration["ConnectionStrings:SQLite_Connection"]));
+builder.Services.AddIdentity<AppUser, AppRole>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 0;
+    options.Password.RequiredUniqueChars = 0;
+    options.User.RequireUniqueEmail = true;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+})
+.AddEntityFrameworkStores<IdentityContext>()
+.AddDefaultTokenProviders();
+
+// Çok dilli
+// Startup.cs dosyasındaki ConfigureServices metodu içinde
+
+
+
+
+
+
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseRequestLocalization();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+};
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+IdentitySeedData.IdentityTestUser(app);
+
+
+app.Run();
